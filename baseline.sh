45cfdb1de9f3ddd2aa0471ef22dc53c1d9c30225
zhaohongfei <iioSnail@163.com>
2022-10-21 17:48:31 +0800

    commit

--- baseline.sh
+++ baseline.sh
@@ -14,12 +14,23 @@
 # done
 
 ood_method='nn_euler'
-#ood_method='nn_cosine'
+method='lof'
 for ((split_index=0; split_index<5; ++split_index)); do
     for ((seed=0; seed<1; ++seed)); do
-        python train_bce.py --gpu 0 --method "lof" --seed $seed --split_index $split_index --ood_method $ood_method --dataset multiwoz23
-        python train_bce.py --gpu 0 --method "lof" --seed $seed --split_index $split_index --ood_method $ood_method --dataset mixsnips
-        python train_bce.py --gpu 0 --method "lof" --seed $seed --split_index $split_index --ood_method $ood_method --dataset atis
-        python train_bce.py --gpu 0 --method "lof" --seed $seed --split_index $split_index --ood_method $ood_method --dataset FSPS
+        python train_bce.py --gpu 0 --method $method --seed $seed --split_index $split_index --ood_method $ood_method --dataset multiwoz23 >> output/run.out
+        python train_bce.py --gpu 0 --method $method --seed $seed --split_index $split_index --ood_method $ood_method --dataset mixsnips >> output/run.out
+        python train_bce.py --gpu 0 --method $method --seed $seed --split_index $split_index --ood_method $ood_method --dataset atis >> output/run.out
+        python train_bce.py --gpu 0 --method $method --seed $seed --split_index $split_index --ood_method $ood_method --dataset FSPS >> output/run.out
+    done
+done
+
+ood_method='nn_cosine'
+method='lof'
+for ((split_index=0; split_index<5; ++split_index)); do
+    for ((seed=0; seed<1; ++seed)); do
+        python train_bce.py --gpu 0 --method $method --seed $seed --split_index $split_index --ood_method $ood_method --dataset multiwoz23 >> output/run.out
+        python train_bce.py --gpu 0 --method $method --seed $seed --split_index $split_index --ood_method $ood_method --dataset mixsnips >> output/run.out
+        python train_bce.py --gpu 0 --method $method --seed $seed --split_index $split_index --ood_method $ood_method --dataset atis >> output/run.out
+        python train_bce.py --gpu 0 --method $method --seed $seed --split_index $split_index --ood_method $ood_method --dataset FSPS >> output/run.out
     done
 done
