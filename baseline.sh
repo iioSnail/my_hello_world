bc68956875da67f0a182cf3a59f8c57278cb3cbc
zzzzzz <42801458+wjczf123@users.noreply.github.com>
2022-10-22 19:19:05 +0800

    Update baseline.sh

--- baseline.sh
+++ baseline.sh
@@ -18,8 +18,29 @@ method='lof'
 for ((split_index=0; split_index<5; ++split_index)); do
     for ((seed=0; seed<5; ++seed)); do
         python train_bce.py --gpu 0 --method $method --seed $seed --split_index $split_index --ood_method $ood_method --dataset multiwoz23 >> output/run.out
+    done
+done
+
+ood_method='nn_euler'
+method='lof'
+for ((split_index=0; split_index<5; ++split_index)); do
+    for ((seed=0; seed<5; ++seed)); do
         python train_bce.py --gpu 0 --method $method --seed $seed --split_index $split_index --ood_method $ood_method --dataset mixsnips >> output/run.out
+    done
+done
+
+ood_method='nn_euler'
+method='lof'
+for ((split_index=0; split_index<5; ++split_index)); do
+    for ((seed=0; seed<5; ++seed)); do
         python train_bce.py --gpu 0 --method $method --seed $seed --split_index $split_index --ood_method $ood_method --dataset atis >> output/run.out
+    done
+done
+
+ood_method='nn_euler'
+method='lof'
+for ((split_index=0; split_index<5; ++split_index)); do
+    for ((seed=0; seed<5; ++seed)); do
         python train_bce.py --gpu 0 --method $method --seed $seed --split_index $split_index --ood_method $ood_method --dataset FSPS >> output/run.out
     done
 done
