32337b3c2d03c05f3c078a9f53c99a20194006ff
zhaohongfei <iioSnail@163.com>
2022-11-07 17:57:01 +0800

    commit

--- model/Classifier_for_CL.py
+++ model/Classifier_for_CL.py
@@ -17,10 +17,12 @@ class Classifier(nn.Module):
         self.encoder = BERT(args)
         self.mlp = nn.Linear(self.args.hidden_size, args.ind_intent_num)
 
+        self.projector = nn.Linear(self.args.hidden_size, self.args.hidden_size)
+
     def forward(self, sens, training=False):
         pooled_output, _, _ = self.encoder(sens)
         logit = self.mlp(pooled_output)
-        return logit, None
+        return logit, None, pooled_output
 
     def test(self, sens):
         pooled_output, _, _ = self.encoder(sens)
@@ -32,8 +34,15 @@ class Classifier(nn.Module):
             dropout = self.args.dropout
 
         # 构建正样本
-        sample_0 = F.dropout(embeddings, dropout)
         sample_1 = F.dropout(embeddings, dropout)
+        sample_2 = F.dropout(embeddings, dropout)
+
+        # 构建负样本
+        sample_3 = embeddings
+        sample_4 = embeddings # TODO
+
+        sample_1, sample_2, sample_3, sample_4 = self.projector(torch.concat([sample_1, sample_2, sample_3, sample_4], dim=0))
+
+
 
         pass
-
